4cc33cab4e1f7971c3590847672622a52061dfae
Brijesh Yadav <brijeshy9653@gmail.com>
2025-04-17 16:29:24 +0530

    Added some features in admin panel to make registering or adding new dustbins easy for admin

--- js/admin.js
+++ js/admin.js
@@ -1,16 +1,16 @@
 // Import necessary Firebase functions
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
-import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js";
 
 // Firebase configuration
 const firebaseConfig = {
-    apiKey: "Your API KEY",
-    authDomain: "Your AUTH DOMAIN",
-    projectId: "Your Project Id",
-    storageBucket: "XXXXXXXXX",
-    messagingSenderId: "XXXXXXXXX",
-    appId: "XXXXXXXXXXX",
-    measurementId: "XXXXXXXXX"
+    apiKey: "Enter your firebase API Key here",
+    authDomain: "bin-buddy-4b96f.firebaseapp.com",
+    projectId: "bin-buddy-4b96f",
+    storageBucket: "bin-buddy-4b96f.firebasestorage.app",
+    messagingSenderId: "1085957179948",
+    appId: "1:1085957179948:web:e576dcf625d145e1119f89",
+    measurementId: "G-HL3VRJYME6"
 };
 
 // Initialize Firebase
